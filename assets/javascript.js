60f8f51786245c0850dadbd6ee1c0df91af1f5b5
Brie Bencich <bbencich@hotmail.com>
2020-10-05 17:40:44 +0300

    time on top of page to work

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,14 +2,32 @@ $(document).ready(function(){
     // eventlistener for save button 
     $(".saveBtn").on("click", function() { 
         // get values of value and time 
-        var value = $(this).siblings(".desctiption").val(); 
+        var value = $(this).siblings(".description").val(); 
         var time = $(this).parent().attr("id"); 
 
         // save to localstorage
         localStorage.setItem(time, value); 
     });
 
-    
+    hourUpdater(); 
+
+// see if intervals on time need to be updated 
+var interval = setInterval(hourUpdater, 1500); 
+
+// save hourly schedule to localstorage 
+$("hour-9 .description").val(localStorage.getItem("hour-9"));
+$("hour-10 .description").val(localStorage.getItem("hour-10")); 
+$("hour-11 .description").val(localStorage.getItem("hour-11")); 
+$("hour-12 .description").val(localStorage.getItem("hour-12")); 
+$("hour-13 .description").val(localStorage.getItem("hour-13")); 
+$("hour-14 .description").val(localStorage.getItem("hour-14")); 
+$("hour-15 .description").val(localStorage.getItem("hour-15")); 
+$("hour-16 .description").val(localStorage.getItem("hour-16")); 
+$("hour-17 .description").val(localStorage.getItem("hour-17")); 
+
+//dispaly current day on top of the page 
+$("#currentDay").text(moment().format("dddd, MMMM Do")); 
+}); 
 
     function hourUpdater() { 
         // current number of hours 
@@ -17,7 +35,7 @@ $(document).ready(function(){
 
         //loop each time block 
         $(".time-block").each(function() { 
-            var blockHour = parsenInt($(this).attr("id").split("-")[1]); 
+            var blockHour = parseInt($(this).attr("id").split("-")[1]); 
 
             // see if we are past this time 
 
@@ -36,23 +54,3 @@ $(document).ready(function(){
         }
     });
 } 
-
-hourUpdater(); 
-
-// see if intervals on time need to be updated 
-var interval = setInterval(houUpdater, 1500); 
-
-// save hourly schedule to localstorage 
-$("hour-9 .description").val(localStorage.getItem("hour-9"));
-$("hour-10 .description").val(localStorage.getItem("hour-10")); 
-$("hour-11 .description").val(localStorage.getItem("hour-11")); 
-$("hour-12 .description").val(localStorage.getItem("hour-12")); 
-$("hour-13 .description").val(localStorage.getItem("hour-13")); 
-$("hour-14 .description").val(localStorage.getItem("hour-14")); 
-$("hour-15 .description").val(localStorage.getItem("hour-15")); 
-$("hour-16 .description").val(localStorage.getItem("hour-16")); 
-$("hour-17 .description").val(localStorage.getItem("hour-17")); 
-
-//dispaly current day on top of the page 
-$("currentDay").text(moment().format("dddd, MMMM Do")); 
-}); 
