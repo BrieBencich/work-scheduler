c1b1ab31b1536c430c16a1dd79e3067960dda814
Brie Bencich <bbencich@hotmail.com>
2020-10-05 17:43:17 +0300

    time and day are finsihed in Java

--- assets/javascript.js
+++ assets/javascript.js
@@ -12,18 +12,18 @@ $(document).ready(function(){
     hourUpdater(); 
 
 // see if intervals on time need to be updated 
-var interval = setInterval(hourUpdater, 1500); 
+var interval = setInterval(hourUpdater, 15000); 
 
 // save hourly schedule to localstorage 
-$("hour-9 .description").val(localStorage.getItem("hour-9"));
-$("hour-10 .description").val(localStorage.getItem("hour-10")); 
-$("hour-11 .description").val(localStorage.getItem("hour-11")); 
-$("hour-12 .description").val(localStorage.getItem("hour-12")); 
-$("hour-13 .description").val(localStorage.getItem("hour-13")); 
-$("hour-14 .description").val(localStorage.getItem("hour-14")); 
-$("hour-15 .description").val(localStorage.getItem("hour-15")); 
-$("hour-16 .description").val(localStorage.getItem("hour-16")); 
-$("hour-17 .description").val(localStorage.getItem("hour-17")); 
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10")); 
+$("#hour-11 .description").val(localStorage.getItem("hour-11")); 
+$("#hour-12 .description").val(localStorage.getItem("hour-12")); 
+$("#hour-13 .description").val(localStorage.getItem("hour-13")); 
+$("#hour-14 .description").val(localStorage.getItem("hour-14")); 
+$("#hour-15 .description").val(localStorage.getItem("hour-15")); 
+$("#hour-16 .description").val(localStorage.getItem("hour-16")); 
+$("#hour-17 .description").val(localStorage.getItem("hour-17")); 
 
 //dispaly current day on top of the page 
 $("#currentDay").text(moment().format("dddd, MMMM Do")); 
